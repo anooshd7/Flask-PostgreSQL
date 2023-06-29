85491ec18e32b86570ba592bf5206176d827bf50
Anoosh Damodar <106900915+anooshd7@users.noreply.github.com>
2023-06-29 12:55:41 +0530

    Flask-restful

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import os
 import psycopg2
-from flask import Flask, render_template, request, url_for, redirect
-from dotenv import load_dotenv 
+from flask import Flask, request, render_template, make_response
+from flask_restful import Resource, Api
+from dotenv import load_dotenv
+
 load_dotenv()
 
 app = Flask(__name__)
+api = Api(app)
 
 def get_db_connection():
     conn = psycopg2.connect(host='localhost',
@@ -14,19 +17,25 @@ def get_db_connection():
                             )
     return conn
 
-@app.route("/")
-def index():
-    conn = get_db_connection()
-    cur = conn.cursor()
-    cur.execute('SELECT * FROM emails;')
-    emails = cur.fetchall()
-    cur.close()
-    conn.close()
-    return render_template('index.html', emails=emails)
+class Home(Resource):
+    def get(self):
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute('SELECT * FROM emails;')
+        emails = cur.fetchall()
+        cur.close()
+        conn.close()
+        headers = {'Content-Type': 'text/html'}
+        return make_response(render_template('index.html', emails=emails),200,headers)
+       
 
-@app.route("/postemails/",methods=('GET','POST'))
-def post_emails():
-    if request.method == "POST":    
+class Emails(Resource):
+
+    def get(self):
+        headers = {'Content-Type': 'text/html'}
+        return make_response(render_template('emails.html'),200,headers)
+
+    def post(self):
         email = request.form['email']
         name = request.form['name']
         conn = get_db_connection()
@@ -37,10 +46,31 @@ def post_emails():
         conn.commit()
         cur.close()
         conn.close()
-    
-    return render_template('emails.html')
+        headers = {'Content-Type': 'text/html'}
+        return make_response(render_template('emails.html'),200,headers)
+
+api.add_resource(Home, '/')
+api.add_resource(Emails, '/emails/')
 
 if __name__ == "__main__":
     app.run(debug=True)
 
+    
+'''   TASKS
+decouple - HOLD(nginx?)
+joins - 
+foreign keys - 
+nullible - 
+uniqueness - 
+flask restful - DONE
+mypy pydocstyle flake8 - 
+sqlalchemy alembic - 
+'''
 
+'''
+POSTGRES:
+
+Table 1: ID(Primary Key) Name(Owner) Email Phone_Number
+Table 2: Car_name
+
+'''
